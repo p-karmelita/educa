c70ae7465c98a0c31b77f158d3484fcd2aa20c18
Piotr Karmelita <piotr@karmelita.eu>
2023-10-16 21:39:38 +0200

    Create public area to display courses.

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -6,7 +6,9 @@ from django.views.generic.base import TemplateResponseMixin, View
 from django.forms.models import modelform_factory
 from django.apps import apps
 from django.urls import reverse_lazy
-from .models import Course, Module, Content
+from django.db.models import Count
+from django.views.generic.detail import DetailView
+from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
@@ -177,3 +179,26 @@ class ContentOrderView(CsrfExemptMixin,
             Content.objects.filter(id=id,
                                    module__course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'ok'})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(
+            total_courses=Count('courses')
+        )
+        courses = Course.objects.annotate(
+            total_modules=Count('modules')
+        )
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects,
+                                        'subject': subject,
+                                        'courses': courses})
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
