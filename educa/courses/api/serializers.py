14bb738426f908d51be52a85e7031b89ad2946b2
Piotr Karmelita <piotr@karmelita.eu>
2023-10-21 20:11:09 +0200

    working with api

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,5 +1,17 @@
 from rest_framework import serializers
-from courses.models import Subject, Course, Module
+from courses.models import Subject, Course, Module, Content
+
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+    class Meta:
+        model = Content
+        fields = ('order', 'item')
 
 
 class ModuleSerializer(serializers.ModelSerializer):
@@ -21,3 +33,17 @@ class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id', 'title', 'slug']
+
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description', 'contents']
+
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules']
--- educa/courses/api/urls.py
+++ educa/courses/api/urls.py
@@ -14,8 +14,5 @@ urlpatterns = [
     path('subject/<pk>/',
          views.SubjectListView.as_view(),
          name='subject_detail'),
-    path('courses/<pk>/enroll/',
-         views.CourseEnrollView.as_view(),
-         name='course_enroll'),
     path('', include(router.urls)),
 ]
